e06411c938f489a8cb26cb27595d4d16ea6d18ab
dester1 <125505835+dester1@users.noreply.github.com>
2023-03-09 20:30:33 +0300

    Delete demo directory

--- demo/src/main/java/ru/biponline/demo/repo/BookRepo.java
+++
@@ -1,7 +0,0 @@
-package ru.biponline.demo.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.biponline.demo.entity.BookEntity;
-
-public interface BookRepo extends CrudRepository<BookEntity, Long> {
-}
